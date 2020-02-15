74c9cb55985ce0ca1c3fec66028187422ea4f9eb
notRook <tcurry10@uncc.edu>
2020-02-14 19:12:17 -0500

    Added both the edit and delete buttons to program

--- src/main/java/com/uncc/ticket/controller/TicketController.java
+++ src/main/java/com/uncc/ticket/controller/TicketController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
 import org.springframework.web.bind.annotation.*;
+import sun.security.krb5.internal.Ticket;
 
 import javax.validation.Valid;
 import java.security.Principal;
@@ -52,13 +53,13 @@ public class TicketController {
 
     @RequestMapping(value = "/tickets/edit/{id}", method = RequestMethod.GET)
     public String editTicket(Model model,@PathVariable("id") Long id) {
-        // Code here
-        return "redirect:/"; //Remove this line
+        model.addAttribute("ticket",ticketService.findById(id));
+        return "tickets/storeTicket";
     }
 
     @RequestMapping(value = "/tickets/delete/{id}", method = RequestMethod.GET)
     public String deleteTicket(@PathVariable("id") Long id) {
-        // Code here
+        ticketService.deleteById(id);
         return "redirect:/";
     }
 
